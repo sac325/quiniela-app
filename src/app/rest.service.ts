437602cb1051aa26f7263a1fd9847f68409c839a
Sergio <sac325@gmail.com>
2022-11-22 18:06:31 -0300

    listo

--- src/app/rest.service.ts
+++ src/app/rest.service.ts
@@ -11,5 +11,14 @@ export class RestService {
   getTeams(url:string){
     return this.http.get(url);
   }
+  // error 401 redirect function
+  private isNoAutorizado(e:any): boolean {
+    if (e.status == 401 || e.status == 403) {
+      if (e.error.mensaje && e.error.mensaje.indexOf('No autorizado') >= 0) {
+        return true;
+      }
+    }
+    return false;
+  }
 
 }
